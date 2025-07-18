75fb9d5e01b7bae2e0f172744d508bf8ee8ace37
devsamuelv <realalloutmusic@gmail.com>
2025-07-18 01:19:46 -0400

    ?

--- flake.nix
+++ flake.nix
@@ -28,50 +28,66 @@
         );
       in
       {
-        packages.default = pkgs.stdenv.mkDerivation {
-          pname = "teleop-control";
-          version = "1.0.0";
-          src = ./.;
+        packages.default =
+          with pkgs.stdenv;
+          mkDerivation {
+            pname = "teleop-control";
+            version = "1.0.0";
+            src = ./.;
 
-          cmakeFlags = [
-            "-DGTK2_GLIBCONFIG_INCLUDE_DIR=${pkgs.glib.out}/lib/glib-2.0/include"
-            "-DGTK2_GDKCONFIG_INCLUDE_DIR=${pkgs.gtk2.out}/lib/gtk-2.0/include"
-          ];
+            cmakeFlags = [
+              "-DGTK2_GLIBCONFIG_INCLUDE_DIR=${pkgs.glib.out}/lib/glib-2.0/include"
+              "-DGTK2_GDKCONFIG_INCLUDE_DIR=${pkgs.gtk2.out}/lib/gtk-2.0/include"
+            ];
 
-          nativeBuildInputs = [
-            pkgs.cmake
-            pkgs.makeWrapper
-            pkgs.glib
-            pkgs.gtk2
-            (pkgs.opencv.override {
-              enableJPEG = true;
-              enableFfmpeg = true;
-            })
+            nativeBuildInputs = [
+              pkgs.cmake
+              pkgs.makeWrapper
+              pkgs.glib
+              pkgs.gtk2
+              (pkgs.opencv.override {
+                enableJPEG = true;
+                enableFfmpeg = true;
+              })
 
-            # x86_64-linux only
-            torch.packages.x86_64-linux.libtorch
-            httplib.packages.x86_64-linux.cpp-httplib
-          ];
-          buildPhase = ''
-            mkdir -p build
-            cmake -S $src -B ./build -DCMAKE_BUILD_TYPE=Release
-            cmake --build build
-          '';
-          installPhase = ''
-            mkdir -p $out/bin
-            cp build/teleop-control $out/bin
-          '';
-        };
+              # x86_64-linux only
+              torch.packages.x86_64-linux.libtorch
+              httplib.packages.x86_64-linux.cpp-httplib
+            ];
+            buildPhase = ''
+              mkdir -p build
+              cmake -S $src -B ./build -DCMAKE_BUILD_TYPE=Release
+              cmake --build build
+            '';
+            installPhase = ''
+              mkdir -p $out/bin
+              cp build/teleop-control $out/bin
+            '';
+          };
 
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            pkgs.cmake
-            pkgs.gcc
-            pkgs.clang
-            pkgs.opencv
-            pkgs.gdb
-          ];
-        };
+        devShells.default =
+          with pkgs;
+          mkShell {
+            GTK2_GLIBCONFIG_INCLUDE_DIR = "${pkgs.glib.out}/lib/glib-2.0/include";
+            GTK2_GDKCONFIG_INCLUDE_DIR = "${pkgs.gtk2.out}/lib/gtk-2.0/include";
+
+            packages = [
+              pkgs.cmake
+              pkgs.gcc
+              pkgs.glib
+              pkgs.gtk2
+              pkgs.clang
+              (pkgs.opencv.override {
+                enableJPEG = true;
+                enableFfmpeg = true;
+              })
+
+              # x86_64-linux only
+              torch.packages.x86_64-linux.libtorch
+              httplib.packages.x86_64-linux.cpp-httplib
+              pkgs.gdb
+            ];
+          };
       }
     );
 }
